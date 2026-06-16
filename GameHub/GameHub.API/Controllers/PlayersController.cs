967f15c84036d2d143e4f9a30b69ab722404ce26
UlyssesSilveiraPita <ulyssespita0@gmail.com>
2026-06-16 14:03:18 -0300

    git inity Gruds entities
    Realizado a programacao dos end points dos cruds ara achievements e players

---
+++ GameHub/GameHub.API/Controllers/PlayersController.cs
@@ -0,0 +1,127 @@
+﻿using GameHub.API.Data;
+using GameHub.API.Dtos.Players;
+using GameHub.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GameHub.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PlayersController : ControllerBase
+{
+    private readonly GameHubDbContext _context; // acessa o banco de dados
+
+    public PlayersController(GameHubDbContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet] // pega todos os player do banco de dados
+    public async Task<ActionResult<IEnumerable<PlayerResponseDto>>> Getall()
+    {
+        var players = await _context.Players
+            .Where(p => !p.IsDeleted)
+            .Select(p => new PlayerResponseDto
+            {
+                Id = p.Id,
+                NickName = p.NickName,
+                Level = p.Level,
+                Experience = p.Experience,
+                CreatedAt = p.CreatedAt
+            })
+            .ToListAsync();
+
+        return Ok(players);
+        
+    }
+
+    [HttpPost] // Criacao do Player 
+    public async Task<ActionResult<PlayerResponseDto>> Create(CreatePlayerDto dto)
+    {
+        var player = new Player
+        {
+            Id = Guid.NewGuid(),
+            NickName = dto.NickName,
+            Level = 1,
+            Experience = 0,
+            CreatedAt = DateTime.UtcNow,
+            IsDeleted = false,
+            UserId = string.Empty
+        };
+
+        _context.Players.Add(player);
+        await _context.SaveChangesAsync();
+
+        var response = new PlayerResponseDto
+        {
+            Id = player.Id,
+            NickName = player.NickName,
+            Level = player.Level,
+            Experience = player.Experience, 
+            CreatedAt = DateTime.UtcNow,    
+        };
+
+
+        return CreatedAtAction(nameof(GetById), new {id = player.Id }, response);
+    }
+
+    [HttpGet("{id}")] // pega player pelo Id
+    public async Task<ActionResult<PlayerResponseDto>> GetById(Guid id)
+    {
+        var player = await _context.Players.FindAsync(id);
+
+        if (player is null || player.IsDeleted)
+        {
+            return NotFound();
+        }
+
+        var response = new PlayerResponseDto
+        {
+            Id = player.Id,
+            NickName = player.NickName,
+            Level = player.Level,
+            Experience = player.Experience,
+            CreatedAt = player.CreatedAt
+
+        };
+
+        return Ok(response);
+    }
+
+    [HttpPut("{id}")] // atualizad o jogador
+    public async Task<ActionResult> Update(Guid id, UpdatePlayerDto dto)
+    {
+        var player = await _context.Players.FindAsync(id);
+
+        if(player is null || player.IsDeleted)
+        {
+            return NotFound();
+        }
+
+        player.NickName = dto.NickName;
+
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")] //marca que foi deletado mas permanece no banco de dados
+    public async Task<ActionResult> Delete(Guid id)
+    {
+        var player = await _context.Players.FindAsync(id);
+
+        if( player is null || player.IsDeleted)
+        {
+            return NoContent();
+        }
+
+        player.IsDeleted = true; 
+
+        await _context.SaveChangesAsync();
+        
+        return NoContent();
+    }
+
+}
--- GameHub/GameHub.API/obj/Debug/net8.0/GameHub.API.AssemblyInfo.cs
+++ GameHub/GameHub.API/obj/Debug/net8.0/GameHub.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameHub.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+444a445732e2bec3f0ecf08d2419a22cf7d6cc94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13c9759c3911003e20906b204b8cb345cfa5707b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameHub.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameHub.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
