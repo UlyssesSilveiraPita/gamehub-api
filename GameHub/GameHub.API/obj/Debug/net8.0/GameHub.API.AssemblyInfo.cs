08994a6fa4c4b0433043be0fa7e32ac6179108a1
UlyssesSilveiraPita <ulyssespita0@gmail.com>
2026-06-18 00:25:22 -0300

    git Inity Add to README
    docs: add project screenshots to README
    
    Projeto finalizado API GameHub

--- GameHub/GameHub.API/obj/Debug/net8.0/GameHub.API.AssemblyInfo.cs
+++ GameHub/GameHub.API/obj/Debug/net8.0/GameHub.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameHub.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25241e9c0ea2e20c86a20423939d8e35cd4c8147")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81aa583bb8b677050b8836f7ba12ce58c51a1fb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameHub.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameHub.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
